9d974189c85264964e474097c34826f7376d6b86
Germán Delbianco <gad@irif.fr>
2018-03-06 14:57:18 +0100

    blah
    some stupid properties of CO. factored out the fact tht trans + irr always gives anti

--- sub.v
+++ sub.v
@@ -22,32 +22,41 @@ Variables (A B V: relation X).
 
 Definition irreflexive (f : relation X) := forall x, f x x -> False.
 
-Definition contains (f g : relation X) :=  forall x y, f x y -> g x y.
-
-Hypothesis AinV: contains A V.
-Hypothesis BinV: contains B V.
+Hypothesis AinV: inclusion X A V.
+Hypothesis BinV: inclusion X B V.
 
 Hypothesis transV: transitive X V.
 Hypothesis irrV: irreflexive V.
-Hypothesis antiV: antisymmetric X V.
 
 Hypothesis transA: transitive X A.
 Hypothesis irrA: irreflexive A.
-Hypothesis antiA: antisymmetric X A.
 
 Definition AUBT : relation X := clos_trans _ (union _ A B).
 
-(* This holds trivially then *) 
+(* Transitive + IRR => Anti *) 
 
-Lemma AUBTinV : contains AUBT V.
+Lemma antiV: antisymmetric _ V.
+Proof. move=>x y /transV H1 /H1 /irrV //. Qed.
+
+Lemma AUBTinV : inclusion _ AUBT V.
 Proof.  
 move=> x y /= H; elim:H=>{x y}x0 y0; first by case; [move/AinV|move/BinV].
 by move=>z _ /transV H1 _ /H1.
 Qed.
 
-(* AUBT cannot have cycles, because V does not ahve them *)
+(* AUBT cannot have cycles, because V does not have them *)
 
 Lemma irrAUBT : irreflexive AUBT.
 Proof. by move=>x /= /AUBTinV /irrV. Qed.
 
+(* AUBT trans: this should be trivial *)
+
+Lemma transAUBT : transitive _ AUBT.
+Proof. by exact:t_trans. Qed.
+
+(* AUBT anti: this is trivial as it is irreflexive and transitive*)
+
+Lemma antiAUBT : antisymmetric _ AUBT.
+Proof. move=>x y /transAUBT H2 /H2 /irrAUBT //. Qed. 
+  
 End RelFacts.
